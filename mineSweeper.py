c53892a048f1342a25c88761f15615c66b581c39
gvs0709 <gvillaress@yahoo.com.br>
2017-08-28 18:31:02 -0300

    Add files via upload

--- mineSweeper.py
+++ mineSweeper.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+##############################################
+#Nome: Gabriel Villares Silveira - 114089936 #
+#      Filipe Jose Maciel Ramalho - 114032785#
+#      Leonardo Santana Dantas - 116052333   # 
+#      Mauricio Lima de Miranda - 1130497797 #
+#                                            #
+##############################################
+
 import numpy as np
 from itertools import chain, combinations
 from os import linesep
@@ -17,21 +28,6 @@ class Grid():
         self.x=x #Number of mines
         self.grid=[]
         self.grid2=[]
-
-    '''def getSize(self): #Returns number of rows, columns and mines
-        m=self.m
-        n=self.n
-        x=self.x
-
-        return (m, n, x)
-
-    def gridSize(self): #Shows the grid size and the number of mines
-        m=self.m
-        n=self.n
-        x=self.x
-
-        print("Grid Size:", m, "x", n)
-        print("Mines:", x)'''
 
     def setGrid(self): #Creates the grids and places mines
         m=self.m
@@ -61,28 +57,15 @@ class Grid():
 
         self.grid=grid
         self.grid2=grid2
-        print(grid)
+        #print(grid)
         #print(grid2)
 
-    def mineScan(self):
+    def mineScan(self, l, c):
         m=self.m
         n=self.n
         grid=self.grid
         grid2=self.grid2
-
-        l=int(input("Select line to open:"))
-
-        while l<0 or l>(m-1): #Checks if l is smaller or bigger than m
-            message1="This line is out of the grid. Lines goes from 0 to {}".format(m-1)
-            print(message1)
-            l=int(input("Select line to open:"))
-
-        c=int(input("Select column to open:"))
-
-        while c<0 or c>(n-1): #Checks if c is smaller or bigger than n
-            message2="This column is out of grid. Columns goes from 0 to {}".format(n-1)
-            print(message2)
-            c=int(input("Select column to open:"))
+        coordinates=[]
 
         if grid[l, c]==1:
             grid2[l, c]=9 #Nine indicates a mine, because its an impossible value for counter
@@ -91,43 +74,53 @@ class Grid():
             print("BOOM!")
             print("Game Over")
             print(grid2)
-            return -1
+            return -1, coordinates
 
         else: #Processes not mined titles and the border conditions
             counter=0
 
             if l==0 and c!=0 and c!=(n-1):
                 counter=grid[l, (c-1)]+grid[l, (c+1)]+grid[(l+1), (c-1)]+grid[(l+1), c]+grid[(l+1), (c+1)]
-
+                coordinates=[[l, (c-1)], [l, (c+1)], [(l+1), (c-1)], [(l+1), c], [(l+1), (c+1)]]
+                
             elif c==0 and l!=0 and l!=(m-1):
                 counter=grid[(l-1), c]+grid[(l-1), (c+1)]+grid[l, (c+1)]+grid[(l+1), c]+grid[(l+1), (c+1)]
+                coordinates=[[(l-1), c], [(l-1), (c+1)], [l, (c+1)], [(l+1), c], [(l+1), (c+1)]]
 
             elif l==0 and c==0:
                 counter=grid[l, (c+1)]+grid[(l+1), c]+grid[(l+1), (c+1)]
+                coordinates=[[l, (c+1)], [(l+1), c], [(l+1), (c+1)]]
 
             elif l==0 and c==(n-1):
                 counter=grid[l, (c-1)]+grid[(l+1), (c-1)]+grid[(l+1), c]
+                coordinates=[[l, (c-1)], [(l+1), (c-1)], [(l+1), c]]
 
             elif l==(m-1) and c!=0 and c!=(n-1):
                 counter=grid[(l-1), (c-1)]+grid[(l-1), c]+grid[(l-1), (c+1)]+grid[l, (c-1)]+grid[l, (c+1)]
+                coordinates=[[(l-1), (c-1)], [(l-1), c], [(l-1), (c+1)], [l, (c-1)], [l, (c+1)]]
 
             elif c==(n-1) and l!=0 and l!=(m-1):
                 counter=grid[(l-1), (c-1)]+grid[(l-1), c]+grid[l, (c-1)]+grid[(l+1), (c-1)]+grid[(l+1), c]
+                coordinates=[[(l-1), (c-1)], [(l-1), c], [l, (c-1)], [(l+1), (c-1)], [(l+1), c]]
 
             elif l==(m-1) and c==0:
                 counter=grid[(l-1), c]+grid[(l-1), (c+1)]+grid[l, (c+1)]
+                coordinates=[[(l-1), c], [(l-1), (c+1)], [l, (c+1)]]
 
             elif l==(m-1) and c==(n-1):
                 counter=grid[(l-1), (c-1)]+grid[(l-1), c]+grid[l, (c-1)]
+                coordinates=[[(l-1), (c-1)], [(l-1), c], [l, (c-1)]]
 
             else:
                 counter=grid[(l-1), (c-1)]+grid[(l-1), c]+grid[(l-1), (c+1)]+grid[l, (c-1)]+grid[l, (c+1)]+grid[(l+1), (c-1)]+grid[(l+1), c]+grid[(l+1), (c+1)]
+                coordinates=[[(l-1), (c-1)], [(l-1), c], [(l-1), (c+1)], [l, (c-1)], [l, (c+1)], [(l+1), (c-1)], [(l+1), c], [(l+1), (c+1)]]
 
             grid2[l, c]=counter
             self.grid2[l, c]=grid2[l, c]
 
-            print(grid2)
-            return counter
+            #print(grid2)
+            return counter, coordinates
+
 
 def tau(m, n, x):
     prop=[] #List of all possible mined tiles
@@ -170,7 +163,7 @@ def user_interface():
 
     possible_grids=tau(m, n, x) #Generate all Possible Grids (Tau Intepretations)
 
-    print("\nPossible Grids: ")
+    print("\nPossible Grids (Tau interpretations): ")
     print_grid(possible_grids) #Display Possible Grids
 
     # -------- Part B --------
@@ -178,32 +171,52 @@ def user_interface():
     g=Grid(m, n, x)
     g.setGrid()
 
-    np.random.seed()
+    '''np.random.seed()
     tile_line=np.random.randint(m) #Chooses a random grid line
     tile_column=np.random.randint(n) #Chooses a random grid column
 
     if g.grid[tile_line, tile_column]==1: #Select tile that does not contain a mine
         while g.grid[tile_line, tile_column]==1:
             tile_line=np.random.randint(m)
-            tile_column=np.random.randint(n)
+            tile_column=np.random.randint(n)'''
+            
+    print("\nPlease enter a tile that does not contain a mine.")
 
-    '''print("\nPlease enter a tile that does not contain a mine.")
-    tile_line = int(input("Tile line: "))
-    tile_column = int(input("Tile column: "))'''
+    tile_line=int(input("Tile line: "))
+    
+    while tile_line<0 or tile_line>(m-1): #Checks if l is smaller or bigger than m
+            message1="This line is out of the grid. Lines goes from 0 to {}".format(m-1)
+            print(message1)
+
+            tile_line=int(input("Select line to open:"))
+    
+    tile_column=int(input("Tile column: "))
+    
+    while tile_column<0 or tile_column>(n-1): #Checks if c is smaller or bigger than n
+            message2="This column is out of grid. Columns goes from 0 to {}".format(n-1)
+            print(message2)
+
+            tile_column=int(input("Select column to open:"))
 
     conditional_grids=generate_grid_conditional(tile_line, tile_column, possible_grids) #Generate All Possible Grids whose specified tile does not have a mine
 
-    message_2="\nPossible grids without mine at tile {}x{}:".format(tile_line, tile_column)
+    message_2="\nPossible grids without mine at tile ({}, {}):".format(tile_line, tile_column)
     print(message_2)
     print_grid(conditional_grids) #Print Possible Grids without specified tile
 
     # -------- Part C --------
+    
+    k, coordinate=g.mineScan(tile_line, tile_column)
+    
+    if k!=-1:
+        answer=list(combinations(coordinate, k))
+        #print '\n'answer
+        
+        formula=str(answer).replace('],','] Λ')
+        formula2=str(formula).replace('),', ') V')
 
-    #letra c aqui!!!
+        print("\nFormula:")
+        print(formula2)
 
 
-#tau=tau()
-#g=Grid(3, 3, 2)
-#g.setGrid()
-#g.mineScan()
 user_interface()
